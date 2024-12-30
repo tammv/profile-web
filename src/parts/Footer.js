a85543ab2642fe4e6efeedc66fe136dfb3c199e2
Tammv <maivantam1978@gmail.com>
2024-12-30 14:07:46 +0700

    footer

--- src/parts/Footer.js
+++ src/parts/Footer.js
@@ -10,20 +10,16 @@ export default function Footer() {
         <div className="flex flex-col sm:flex-row mt-8 justify-center">
           <div className="w-1/3 flex-col ml-16 mr-8">
             <BrandIcon />
-            <p className="w-full text-lg text-gray-400 font-light">
-              Growing Your Business <br />
-              Is Our Calling
-            </p>
           </div>
           <div className="w-1/3 mt-0 ml-16 mr-0 sm:ml-0 sm:mr-5">
             <h1 className="text-lg text-theme-blue pt-4 pb-2">Office</h1>
-            <p className="text-lg text-gray-400 font-light">rac.hafidz@gmail.com</p>
-            <p className="text-lg text-gray-400 font-light">Klaten, Central Java, Indonesia</p>
+            <p className="text-lg text-gray-400 font-light">maivantam1878@gmail.com</p>
+            <p className="text-lg text-gray-400 font-light">Danang, Vietnam</p>
           </div>
           <div className="w-1/3 ml-16 sm:ml-0 mt-0">
             <h1 className="text-lg text-theme-blue pt-4 pb-2">Social</h1>
             <Button
-              href="https://www.instagram.com/racmathafidz/"
+              href="https://www.instagram.com/tom_quinter/"
               type="link"
               target="_blank"
               className="flex text-lg text-gray-400 font-light hover:underline"
@@ -32,7 +28,7 @@ export default function Footer() {
               Instagram
             </Button>
             <Button
-              href="https://www.linkedin.com/in/racmat-hafidz-89982a156/"
+              href="https://www.linkedin.com/in/iammvt/"
               type="link"
               target="_blank"
               className="flex text-lg text-gray-400 font-light hover:underline"
@@ -41,7 +37,7 @@ export default function Footer() {
               LinkedIn
             </Button>
             <Button
-              href="https://github.com/racmathafidz"
+              href="https://github.com/tammv"
               type="link"
               target="_blank"
               className="flex text-lg text-gray-400 font-light hover:underline"
@@ -52,17 +48,17 @@ export default function Footer() {
           </div>
         </div>
         <div className="flex-col text-center mt-7">
-          <p className="text-lg text-gray-400 font-light">Copyright 2021 - All rights reserved - Racxstudio</p>
+          <p className="text-lg text-gray-400 font-light">Copyright 2024 - All rights reserved - Tom Quinter</p>
           <div className="flex-row">
             <p className="inline-block text-lg text-gray-400 font-light">Made with &#x2764; by&nbsp;</p>
             <Button
-              href="https://github.com/racmathafidz"
+              href="https://github.com/tammv"
               type="link"
               target="_blank"
               className="text-lg text-theme-purple font-light"
               isExternal
             >
-              Racmat Hafidz Fadli
+              Mai Van Tam
             </Button>
           </div>
         </div>
